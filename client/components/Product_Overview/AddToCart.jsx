8c8beb20721aa15f499e8a64ddfc3592ca940014
Alex Duncan <monkeymedic26@gmail.com>
2021-03-01 15:39:24 -0800

    all product info, buttons, selectors, and images rendering

--- client/components/Product_Overview/AddToCart.jsx
+++ client/components/Product_Overview/AddToCart.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 
 const AddToCart = () => (
   <div>
-    buttons, lots of buttons
     <select>
       <option value="size">Select Size</option>
     </select>
     <select>
       <option value="quantity">1</option>
     </select>
+    <button type="submit">ADD TO BAG</button>
+    <button type="submit">Heart, Star, Plus thingy</button>
   </div>
 );
 
