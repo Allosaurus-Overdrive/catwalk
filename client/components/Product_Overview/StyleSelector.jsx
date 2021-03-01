9c551cf80a83e4fd3fb4fff4b7904236d72273ea
Alex Duncan <monkeymedic26@gmail.com>
2021-03-01 15:56:53 -0800

    Installed styled-components

--- client/components/Product_Overview/StyleSelector.jsx
+++ client/components/Product_Overview/StyleSelector.jsx
@@ -1,9 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
+import config from '../../../server/config';
 
-const StyleSelector = () => (
+const options = {
+  headers: {
+    Authorization: `${config.TOKEN}`,
+  },
+};
+
+const StyleSelector = () => {
+  const [thumbnail, setThumbnail] = useState('');
+
+  const getThumbnail = () => axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products/20111/styles', options)
+    .then((response) => (
+      setThumbnail(response.data.results[0].photos[0].thumbnail_url)
+    ))
+
+  getThumbnail();
+
+  return (
   <div>
-    <button type="button" className="style">style 1</button>
+    <button type="button" className="style">{thumbnail}</button>
   </div>
-);
+  );
+};
 
 export default StyleSelector;
