c9a1cc9153c01bde192126d0e2b53c5401bd0502
Dani Tian <td.tiandani@gmail.com>
2021-03-01 17:22:20 -0800

    dynamically renders array of product cards with example data

--- client/components/related/related.jsx
+++ client/components/related/related.jsx
@@ -1,7 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
 
-// need to get the productID of the current page. Maybe from alex's overview component where he stores the id?
+// TO-DOS:
+// get the productID of the current page from alex's overview component where he stores the id?
+// import the ratings star function from sheeba
+// store state of
 
 const exampleGetProduct = {
   id: 20111,
@@ -78,16 +81,138 @@ const exampleGetStyles = {
   ],
 };
 
+const exampleProducts = [
+  {
+    id: 20111,
+    campus: 'hr-sea',
+    name: 'Murl Dress',
+    slogan: 'Non ducimus maxime.',
+    description: 'Distinctio nostrum odit mollitia qui. Officia veritatis a aut velit et laudantium. Repellendus sint voluptatem et.',
+    category: 'Dress',
+    default_price: 817.00,
+    created_at: '2021-02-24T19:34:41.281Z',
+    updated_at: '2021-02-24T19:34:41.281Z',
+    features: [
+      {
+        feature: 'Cut',
+        value: 'Skinny',
+      },
+      {
+        feature: 'Fair Trade Certified',
+        value: null,
+      },
+      {
+        feature: 'Non-GMO',
+        value: null,
+      },
+      {
+        feature: 'Lens',
+        value: '"100% UV Protective"',
+      },
+    ],
+  },
+  {
+    id: 20222,
+    campus: 'hr-sea',
+    name: 'Another Shirt',
+    slogan: 'Non ducimus maxime.',
+    description: 'Distinctio nostrum odit mollitia qui. Officia veritatis a aut velit et laudantium. Repellendus sint voluptatem et.',
+    category: 'Top',
+    default_price: 820.00,
+    created_at: '2021-02-24T19:34:41.281Z',
+    updated_at: '2021-02-24T19:34:41.281Z',
+    features: [
+      {
+        feature: 'Cut',
+        value: 'Skinny',
+      },
+      {
+        feature: 'Fair Trade Certified',
+        value: null,
+      },
+      {
+        feature: 'Non-GMO',
+        value: null,
+      },
+      {
+        feature: 'Lens',
+        value: '"100% UV Protective"',
+      },
+    ],
+  },
+  {
+    id: 20333,
+    campus: 'hr-sea',
+    name: 'Some Pants',
+    slogan: 'Non ducimus maxime.',
+    description: 'Distinctio nostrum odit mollitia qui. Officia veritatis a aut velit et laudantium. Repellendus sint voluptatem et.',
+    category: 'Bottom',
+    default_price: 820.00,
+    created_at: '2021-02-24T19:34:41.281Z',
+    updated_at: '2021-02-24T19:34:41.281Z',
+    features: [
+      {
+        feature: 'Cut',
+        value: 'Skinny',
+      },
+      {
+        feature: 'Fair Trade Certified',
+        value: null,
+      },
+      {
+        feature: 'Non-GMO',
+        value: null,
+      },
+      {
+        feature: 'Lens',
+        value: '"100% UV Protective"',
+      },
+    ],
+  },
+  {
+    id: 20555,
+    campus: 'hr-sea',
+    name: 'A Purse',
+    slogan: 'Non ducimus maxime.',
+    description: 'Distinctio nostrum odit mollitia qui. Officia veritatis a aut velit et laudantium. Repellendus sint voluptatem et.',
+    category: 'Accessory',
+    default_price: 820.00,
+    created_at: '2021-02-24T19:34:41.281Z',
+    updated_at: '2021-02-24T19:34:41.281Z',
+    features: [
+      {
+        feature: 'Cut',
+        value: 'Skinny',
+      },
+      {
+        feature: 'Fair Trade Certified',
+        value: null,
+      },
+      {
+        feature: 'Non-GMO',
+        value: null,
+      },
+      {
+        feature: 'Lens',
+        value: '"100% UV Protective"',
+      },
+    ],
+  },
+];
+
 // single product card component, given what it has from props
 // use example data for now
 // write styled components outside of the actual functional component itself...
 
-const RelatedCardWrapper = styled.section`
+// RELATED PRODUCT CARD STYLED COMPONENTS
+
+const RelatedCardWrapper = styled.li`
   font-family: 'Roboto', sans-serif;
+  display: inline-block;
   width: 200px;
   height: 300px;
   background: white;
-  margin: auto;
+  margin: auto 10px;
   border-width: 1px;
   border-style: solid;
   border-color: black;
@@ -107,6 +232,17 @@ const RelatedImage = styled.img`
   width: 100%;
   background: no-repeat center center;
   object-fit: cover;
+`;
+
+const RelatedIcon = styled.i`
+  color: white;
+  position: absolute;
+  right: 10px;
+  top: 10px;
+  opacity: 1;
+  &:hover{
+    color: gold;
+  }
 `;
 
 const RelatedOverview = styled.section`
@@ -137,13 +273,54 @@ const RelatedPrice = styled.h3`
   margin-block-end: 0.5em;
 `;
 
+// RELATED PRODUCT LIST STYLED COMPONENTS
+
+const RelatedProductsWrapper = styled.section`
+  font-family: 'Roboto', sans-serif;
+  display: inline-flex;
+  height: 300px;
+  width: 80%;
+  background: white;
+  margin: 10px;
+  position: relative;
+  overflow: hidden;
+  white-space: nowrap;
+`;
+
+const RelatedProductsList = styled.ul`
+  display: inline;
+  padding-inline-start: 0;
+  margin: auto;
+  position: relative;
+`;
+
+const RelatedLeftButton = styled.button`
+  font-size: 150%;
+  float: left;
+  margin: 125px 0px;
+  padding: 0;
+  height: 50px;
+  width: 50px;
+`;
+
+const RelatedRightButton = styled.button`
+  font-size: 150%;
+  float: right;
+  margin: 125px 0px;
+  padding: 0;
+  height: 50px;
+  width: 50px;
+`;
+
+// one product card
 function ProductCard(props) {
   return (
     <RelatedCardWrapper className="related-card-wrapper">
+      <RelatedIcon className="far fa-star" />
       <RelatedImage className="related-image" src={exampleGetStyles.results[0].photos[0].thumbnail_url} alt="Model wearing selected style" />
       <RelatedOverview className="related-overview">
-        <RelatedCategory className="related-category">{exampleGetProduct.category}</RelatedCategory>
-        <RelatedName className="related-name">{exampleGetProduct.name}</RelatedName>
+        <RelatedCategory className="related-category">{props.item.category}</RelatedCategory>
+        <RelatedName className="related-name">{props.item.name}</RelatedName>
         <RelatedPrice className="related-price">
           $
           {exampleGetStyles.results[0].original_price}
@@ -155,11 +332,21 @@ function ProductCard(props) {
 }
 
 // list of product cards
-// function RelatedProducts(props) {
-//   return (
-
-//   );
-// }
+function RelatedProducts(props) {
+  return (
+    <div>
+      <RelatedLeftButton className="left" type="button"> &#8592; </RelatedLeftButton>
+      <RelatedProductsWrapper>
+        <RelatedProductsList>
+          {exampleProducts.map((item) => (
+            <ProductCard item={item} key={item.id} />
+          ))}
+        </RelatedProductsList>
+      </RelatedProductsWrapper>
+      <RelatedRightButton className="right" type="button"> &#8594; </RelatedRightButton>
+    </div>
+  );
+}
 
 // export whole list of related products at the end, update the export
-export default ProductCard;
+export default RelatedProducts;
