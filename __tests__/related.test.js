0117e744a5e5be4862b854b4f9f5c67af5fa3bda
Dani Tian <td.tiandani@gmail.com>
2021-03-02 16:10:43 -0800

    minor update to test file

--- __tests__/related.test.js
+++ __tests__/related.test.js
@@ -1,9 +1,4 @@
-import React from 'react';
-import { render, screen, cleanup } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 import Related from '../client/components/related/related';
-
-afterEach(cleanup);
 
 describe('Test function', () => {
   it('Should subtract one number from another', () => {
