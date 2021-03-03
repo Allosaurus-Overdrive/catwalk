41f4cd717cb6bcf91f095fb482ebda6a309b6a42
Alex Duncan <monkeymedic26@gmail.com>
2021-03-02 20:02:31 -0800

    Basic css setup.  Next goal is to start on the selector functionality

--- client/components/Product_Overview/ProductInfo.jsx
+++ client/components/Product_Overview/ProductInfo.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import config from '../../../server/config';
 
@@ -9,40 +9,29 @@ const options = {
 };
 
 const ProductInfo = () => {
-  const [description, setDescription] = useState('');
   const [name, setName] = useState('');
   const [category, setCategory] = useState('');
   const [price, setPrice] = useState();
-  const [slogan, setSlogan] = useState('');
-  const [features, setFeatures] = useState([]);
+
   const getInfo = () => axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-sea/products/20111', options)
     .then((response) => {
       setName(response.data.name);
       setCategory(response.data.category);
       setPrice(response.data.default_price);
-      setSlogan(response.data.slogan);
-      setDescription(response.data.description);
-      setFeatures(response.data.features);
     })
     .catch((err) => {
       throw err;
     });
 
-  getInfo();
+  useEffect(() => {
+    getInfo();
+  });
 
   return (
     <div>
-      <div>{name}</div>
       <div>{category}</div>
+      <div>{name}</div>
       <div>{price}</div>
-      <div>{description}</div>
-      <div>{slogan}</div>
-      {features.map((feature) => (
-        <div key={feature.feature}>
-          {feature.feature}
-          {feature.value}
-        </div>
-      ))}
     </div>
   );
 };
