1e6c87b43fa693fa934601c66d76b0742aa72fd7
Alex Duncan <monkeymedic26@gmail.com>
2021-03-04 15:09:43 -0800

    Added a ImageSlides component and created carousel with working buttons

--- client/components/Product_Overview/ImageGallery.jsx
+++ client/components/Product_Overview/ImageGallery.jsx
@@ -1,24 +1,22 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import styled from 'styled-components';
+// import styled from 'styled-components';
+import ImageSlide from './ImageSlide';
 
 // **Styling Templates** //
-
-const Picture = styled.img`
-  width: 450px;
-  grid-column: 1;
-  place-self: center;
-  background-color: light grey;
-  `;
 
 // **Functionality Section** //
 
 const ImageGallery = () => {
-  const [image, setImage] = useState('');
+  const [image, setImage] = useState([]);
+  const [currentImage, setCurrentImage] = useState(0);
 
   const getImages = () => axios.get('/products/20111/styles')
     .then((response) => (
-      setImage(response.data.results[0].photos[0].url)
+      response.data.results.map((style) => (
+        console.log(style),
+        setImage(image => [...image, style.photos[0].url])
+      ))
     ))
     .catch((err) => {
       throw err;
@@ -28,8 +26,60 @@ const ImageGallery = () => {
     getImages();
   }, []);
 
+  // **Arrow for Carousel**//
+  const ArrowLeft = ({ direction, clickFunction, glyph, isEdge }) => (
+    <div
+      className={`slide-arrow${direction}`}
+      onClick={clickFunction}
+      onKeyPress={clickFunction}
+      role="button"
+      tabIndex="0"
+    >
+      {glyph}
+    </div>
+  );
+
+  const ArrowRight = ({ direction, clickFunction, glyph }) => (
+    <div
+      className={`slide-arrow${direction}`}
+      onClick={clickFunction}
+      onKeyPress={clickFunction}
+      role="button"
+      tabIndex="0"
+    >
+      {glyph}
+    </div>
+  );
+
+  // **Arrow functionality**//
+  const previousSlide = () => {
+    setCurrentImage(currentImage - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentImage(currentImage + 1);
+  };
+
   return (
-    <Picture src={image} alt="" />
+    <div className="carousel">
+      {currentImage !== 0
+      && (
+        <ArrowLeft
+          direction="left"
+          clickFunction={previousSlide}
+          glyph="&#9664;"
+        />
+      )}
+      <ImageSlide url={image[currentImage]} />
+      {currentImage !== image.length - 1
+      && (
+        <ArrowRight
+          direction="right"
+          clickFunction={nextSlide}
+          glyph="&#9654;"
+        />
+      )}
+    </div>
   );
 };
 
