c68ea4f7ee3c8e1bbd7e2c060e2641d7a49bbf61
Sheeba Nair <sheebanairpaul@gmail.com>
2021-03-05 02:08:10 -0800

    created a progress bar for ratings and product breakdown

---
+++ client/components/Reviews/progressBar.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const ProgressBar = (props) => {
+  const { bgcolor, completed } = props;
+
+  const containerStyles = {
+    height: 15,
+    width: '60%',
+    backgroundColor: "#e0e0de",
+    borderRadius: 30,
+  };
+
+  const fillerStyles = {
+    height: '100%',
+    width: `${completed}%`,
+    backgroundColor: bgcolor,
+    borderRadius: 'inherit',
+    textAlign: 'right',
+  };
+
+  // const labelStyles = {
+  //   // padding: 5,
+  //   color: 'white',
+  //   fontWeight: 'bold',
+  // };
+
+  return (
+    <div style={containerStyles}>
+      <div style={fillerStyles}>
+        {/* <span style={labelStyles}></span> */}
+      </div>
+    </div>
+  );
+};
+
+export default ProgressBar;
