193b1442007e4cc3d69c2f7cd540988fae22c77a
Alex Duncan <monkeymedic26@gmail.com>
2021-03-08 11:55:02 -0800

    Size selector now only shows sizes with quantity > 0

--- client/components/Product_Overview/AddToCart.jsx
+++ client/components/Product_Overview/AddToCart.jsx
@@ -1,6 +1,5 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
-import axios from 'axios';
 
 // **Styling Templates** //
 
