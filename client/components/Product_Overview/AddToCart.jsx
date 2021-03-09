7a48085ca096a07a3c3d0b87d5fdf7115bbaa66e
Alex Duncan <monkeymedic26@gmail.com>
2021-03-08 16:07:29 -0800

    add outfit adder function to the outfit button.  does not cause rerender

--- client/components/Product_Overview/AddToCart.jsx
+++ client/components/Product_Overview/AddToCart.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import styled from 'styled-components';
+import axios from 'axios';
 
 // **Styling Templates** //
 
