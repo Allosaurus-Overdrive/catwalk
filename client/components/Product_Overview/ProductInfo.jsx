19b82e5efe072ee7a6fb43269ad86da0302d93d0
Alex Duncan <monkeymedic26@gmail.com>
2021-03-11 16:21:17 -0800

    deleted some psuedo code

--- client/components/Product_Overview/ProductInfo.jsx
+++ client/components/Product_Overview/ProductInfo.jsx
@@ -40,18 +40,6 @@ const SalePriceStyle = styled.div`
   margin: 7.5px 10px 7.5px 10px;
 `;
 
-// ** Star rating props ** //
-// const yes = Number(recommend.true);
-//   const no = Number(recommend.false);
-//   const total = yes + no;
-//   const percent = Math.round((yes / total) * 100);
-//   // const [filter, setFilter] = useState('All');
-//   // calculating average rating for product
-//   const scores = ratings;
-//   const sum = 1 * (Number(scores['1'])) + 2 * (Number(scores['2'])) + 3 * (Number(scores['3'])) + 4 * (Number(scores['4'])) + 5 * (Number(scores['5']));
-//   const weightedAvg = (sum / total);
-//   const avgRating = Number((weightedAvg).toFixed(1));
-
 // **Functionality Section** //
 
 const ProductInfo = ({
